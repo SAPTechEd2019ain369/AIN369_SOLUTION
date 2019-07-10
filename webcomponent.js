f5e9ce5886dba1193a523129bf705add38d84f26
roadmaptrackerpoc$roadmaptrackerpoc <david.stocker@sap.com>
2019-07-10 13:37:29 +0200

    updated json

--- webcomponent.js
+++ webcomponent.js
@@ -12,7 +12,7 @@
 
     d3Script.onload = () => 
 
-    customElements.define('com-sap-teched-gauge-solution-exe4', class Gauge extends HTMLElement {
+    customElements.define('com-sap-teched-gauge-solution', class Gauge extends HTMLElement {
 
 
         disconnectedCallback () {
